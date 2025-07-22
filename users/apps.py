28b46ef77ce0781d9a35c70b674007c509896888
Mrinal Das <mrinaldas@live.com>
2025-07-22 21:09:44 +0530

    apps.py is like a configuration file for your Django app (users in this case).
    The ready() method is used to run setup code, such as importing signal handlers (used for things like automatically creating a UserProfile when a User is created).
    
    Importing users.signals here ensures that any @receiver functions inside signals.py are activated when the app starts.

--- users/apps.py
+++ users/apps.py
@@ -1,8 +1,17 @@
+# Import the AppConfig base class from Django
 from django.apps import AppConfig
 
+# Define a configuration class for the 'users' app
+# Django uses this class to configure some behaviors of your app
 class UsersConfig(AppConfig):
+    # Set the default type for automatically created primary key fields in models
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # This tells Django the name of the app this config belongs to
     name = 'users'
 
+    # This method runs automatically when the app is ready (i.e., when Django starts up)
     def ready(self):
+        # Import the signals module from this app
+        # This is necessary to make sure your signal handlers are registered at startup
         import users.signals
