9aea4be491704cd7a0482bc650f078bc5d1c75e8
Mrinal Das <mrinaldas@live.com>
2025-07-22 21:13:01 +0530

    This file defines a basic login form using Django's built-in forms.Form class.
    The form contains two fields:
    - username: plain text input.
    - password: input that hides text using PasswordInput widget.
    
    This form will be used in login page to accept user credentials.

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,10 @@
+# Import the forms module from Django
 from django import forms
 
+# Define a custom form for handling user login
 class LoginForm(forms.Form):
+    # A simple text input for the username field
     username = forms.CharField()
+    
+    # A password input field that hides characters as the user types
     password = forms.CharField(widget=forms.PasswordInput)
